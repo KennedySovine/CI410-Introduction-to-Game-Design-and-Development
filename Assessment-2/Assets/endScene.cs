5ffd5a6883e628358cc6dfcfa23a5d37796fb886
KennedySovine <89476714+KennedySovine@users.noreply.github.com>
2023-06-01 13:20:16 +0100

    Working on Fixing Scripts

--- Assessment-2/Assets/endScene.cs
+++ Assessment-2/Assets/endScene.cs
@@ -9,13 +9,10 @@ public class endScene : MonoBehaviour
     public GameObject go_message_panel;
     public GameObject _restartButton;
     private GameObject PC;
-    private GameObject Ending;
 
     public void OnTriggerEnter(Collider collider)
      {
-        Ending = GameObject.Find("Ending");
         PC =GameObject.FindWithTag("Player");
-        Ending.SetActive(true);
         go_message_panel.SetActive(true);
         PC.GetComponent<DD_PC_Control>().enabled = false;
      }
